66e27b0f1882a383929b5bd13a9f30acc8aa9477
AlejandroEPis <alebpis@gmail.com>
2025-11-12 11:25:34 -0300

    sin t

--- js/environment.js
+++ js/environment.js
@@ -1,3 +1,3 @@
 /*Configuracion con AirTable*/
-export const API_TOKEN = "patDkCviasNkhYxtY.af02e1ef06982ce2a1bb03466e73e86a3d9b2540c7479b731be2c571a7ff1616";       
-export const BASE_ID = "app18crwMUYE5KQ1B"; 
+export const API_TOKEN = " ";       
+export const BASE_ID = ""; 
